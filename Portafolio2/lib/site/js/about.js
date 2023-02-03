970f25622bc5e76054d018b2b76d26d3df9e23ec
Andres_a <andres.hoyos18@eia.edu.co>
2023-02-03 13:41:01 -0500

    Created javascripts for pages

---
+++ Portafolio2/lib/site/js/about.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+    // add active class to nav element
+    let activeNavElement = document.getElementById("nav-about");
+    activeNavElement.classList.add("active")
+
+    console.log("Loaded")
+}
---
+++ Portafolio2/lib/site/js/projects.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+    // add active class to nav element
+    let activeNavElement = document.getElementById("nav-projects");
+    activeNavElement.classList.add("active")
+
+    console.log("Loaded")
+}
