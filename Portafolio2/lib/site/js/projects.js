90feab7b5cd80a97132d7a4a8fed307cb68e99c6
Andres Hoyos <andres.hoyos18@eia.edu.co>
2023-02-04 20:12:02 -0500

    Finished projects page.
    Missing connecting to real backend to get projects

--- Portafolio2/lib/site/js/projects.js
+++ Portafolio2/lib/site/js/projects.js
@@ -1,3 +1,14 @@
+import {Project} from "./shared.js";
+
+
+const projects = new Set([
+    new Project("One Punch", "API", "C#", "Asp.Net Core", "<i>REST</i> API sobre <b>One Punch Man</b>.", "https://github.com/AndresH18/OnePunch"),
+    new Project("One Punch", "API", "C#", "Asp.Net Core", "<i>REST</i> API sobre <b>One Punch Man</b>.", "https://github.com/AndresH18/OnePunch"),
+    new Project("One Punch", "API", "C#", "Asp.Net Core", "<i>REST</i> API sobre <b>One Punch Man</b>.", "https://github.com/AndresH18/OnePunch"),
+    new Project("One Punch", "API", "C#", "Asp.Net Core", "<i>REST</i> API sobre <b>One Punch Man</b>.", "https://github.com/AndresH18/OnePunch"),
+    new Project("One Punch", "API", "C#", "Asp.Net Core", "<i>REST</i> API sobre <b>One Punch Man</b>.", "https://github.com/AndresH18/OnePunch"),
+])
+
 window.onload = () => {
     // add active class to nav element
     let activeNavElement = document.getElementById("nav-projects");
@@ -9,7 +20,24 @@ window.onload = () => {
         link.rel = "noreferrer noopener"
     }
 
-    console.log("Loaded")
+    loadProjects()
 
+    console.log("Loaded")
+}
+
+function loadProjects() {
+    const projectsTable = document.getElementById("projects-table")
+
+    // TODO: use api call, use for-of loop
+    projects.forEach(function (key, val) {
+        let row = projectsTable.insertRow()
+
+        row.insertCell(0).innerHTML = val.name
+        row.insertCell(1).innerHTML = val.type
+        row.insertCell(2).innerHTML = val.language
+        row.insertCell(3).innerHTML = val.framework
+        row.insertCell(4).innerHTML = val.description
+        row.insertCell(5).innerHTML = `<a class="btn btn-dark" href="${val.url}">Repositorio</a>`
+    })
 
 }
---
+++ Portafolio2/lib/site/js/shared.js
@@ -0,0 +1,10 @@
+export class Project {
+    constructor(name, type, language, framework, description, url) {
+        this.name = name
+        this.type = type
+        this.language = language
+        this.framework = framework
+        this.description = description
+        this.url = url
+    }
+}
