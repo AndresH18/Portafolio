33b752e8288860dda27b9aafc589000d710d7fcd
Andres Hoyos <andres.hoyos18@eia.edu.co>
2024-01-02 00:25:33 -0500

    Added some documentation

--- Portfolio/Services/ServiceConfigurationExtensions.cs
+++ Portfolio/Services/ServiceConfigurationExtensions.cs
@@ -4,6 +4,14 @@ namespace Portfolio.Services;
 
 public static class ServiceConfigurationExtensions
 {
+    /// <summary>
+    ///     Registers the Application's services
+    /// </summary>
+    /// <param name="services">the IServiceCollection to add the services to.</param>
+    /// <seealso
+    ///     href="https://learn.microsoft.com/en-us/aspnet/core/blazor/fundamentals/dependency-injection?view=aspnetcore-8.0#service-lifetime">
+    ///     Blazor Service Lifetime
+    /// </seealso>
     public static void RegisterGithubServices(this IServiceCollection services)
     {
         services.AddSingleton<IGitHubClient, GitHubClient>(_ => new GitHubClient(new ProductHeaderValue("portfolio")));
